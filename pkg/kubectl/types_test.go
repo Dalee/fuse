324106b59d0c20d1be58c0672bad8cbccb1fe81a
Arkady Emeleyanov <arkady.emelyanov@dalee.ru>
2017-02-15 23:32:27 +0300

    types, more coverage

--- pkg/kubectl/types_test.go
+++ pkg/kubectl/types_test.go
@@ -5,8 +5,13 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestReplicaSet_GetImages(t *testing.T) {
+func TestReplicaSet_Types(t *testing.T) {
 	rs := ReplicaSet{
+		Kind: "ReplicaSet",
+		Metadata: kubeTypeMetadata{
+			Name: "test-replicaset-42",
+			Namespace: "default",
+		},
 		Spec: kubeTypeResourceSpec{
 			Template: kubeTypeTemplate{
 				Spec: kubeTypeSpec{
@@ -20,5 +25,28 @@ func TestReplicaSet_GetImages(t *testing.T) {
 		},
 	}
 
+	assert.Equal(t, "test-replicaset-42", rs.GetName())
+	assert.Equal(t, "replicaset", rs.GetKind())
 	assert.Equal(t, []string{"registry.example.com/example/repo:1"}, rs.GetImages())
+}
+
+func TestDeployment_Types(t *testing.T) {
+	d := Deployment{
+		Kind: "Deployment",
+		Metadata: kubeTypeMetadata{
+			Name: "test-deployment",
+			Namespace: "default",
+		},
+	}
+
+	assert.Equal(t, "test-deployment", d.GetName())
+	assert.Equal(t, "deployment", d.GetKind())
+}
+
+func TestKubeResourceList_Types(t *testing.T) {
+	rl := kubeResourceList{
+		Kind: "List",
+	}
+
+	assert.Equal(t, "list", rl.GetKind())
 }
